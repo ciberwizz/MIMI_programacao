7d41d2c62744091fe3277b8486c0c318f2198c1f
Miguel Rossi Seabra <ciberwizz@ciberwizz.(none)>
2012-12-01 14:53:55 +0000

    initial globals

---
+++ src/mimi.pl
@@ -0,0 +1,37 @@
+# variable structure
+#  
+#    trabalham 5 dias por semana
+#    tem ferias de 11 dias de 6 em 6 meses
+#    renovacao de contracto é gratis
+# 
+#    globais
+#       PUs  -> preco de linha de codigo simples
+#       PUc  -> preco de linha de codigo complexo
+#       C    -> custo de contratacao
+#       Srp  -> ordenado senior permanente 
+#       Src  -> ordenado senior contractado
+#       Jr   -> ordenado junior
+#       Z    -> dias que sendo contractado faz 0
+#       N    -> N linhas de codigo por dia por programador
+#       P    -> numero de programadores senior permanente
+#       E    -> capital inicial
+#       M    -> despesas mensais fixas
+#       A    -> percentagem a descontar no valor do proj 
+#		por cada dia de atraso
+#       Amax -> percentagem maxima de A*dias de atraso
+#       Adias-> maximo de dias de atraso
+#
+# 
+# Encomendas = { [NLinhas,Complexidade, Data_Intermedia, DataFinal],..}
+#
+#
+#
+
+
+
+
+
+
+
+
+
