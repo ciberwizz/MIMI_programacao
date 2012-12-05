4d52ba7e0124638e64bc9f380ff34a28f1818a6e
paulo.f.reis <paulo.f.reis@gmail.com>
2012-12-05 00:57:14 +0000

    O first_attempt.pl já funciona! :)
    Limita o n.º de Seniores, já agora ocorre-me que se usarmos a
    percentagem (0 a 100) podemos respeitar os requisitos do trabalho.
    
    Quanto a durações uso dias, pois aproxima-se mais do pretendido.
    
    DataTermino deve ser um valor alto, mas não em demasiada, talvez
    limitado pela soma das durações de todos os projetos a dividir pelo
    número médio de Seniores. Para já está limitado a 1000.
    
    Crio listas para cada um dos parâmetros, as quais têm de ser preenchidas
    com os valores das encomendas aceites. Nesta versão ainda não tratei
    disso.
    
    A única restrição que aplico é o máximo de Projetos simultâneos (n.º de
    Seniores disponíveis).

--- src/first_attempt.pl
+++ src/first_attempt.pl
@@ -3,61 +3,69 @@
 
 
 fa :-
-        Encomendas = [
+        DataTermino = 1000,             % Data limite para terminar todos os projetos.
+        SenioresDisponiveis = 4,        % Quantidade de Séniores disponíveis a cada momento.
+        Projectos = [
                         [350, 1, 10, 30], [450, 1, 14, 20], [600, 1, 23, 30], [1400, 1, 20, 30], [1000, 1, 22, 30]
                      ],
         
-        length(Encomendas, QtdEnc),
+        length(Projectos, QtdProjetos),         % Verificar quantos projetos são para escalonar.
+        length(DatasInicio, QtdProjetos),       % Lista com as datas de inicio dos projetos.
+        length(DatasFim, QtdProjetos),          % Lista com as datas de fim dos projetos.
+        length(DuracaoProjetos, QtdProjetos),   % Lista com a duração dos projetos.
+        length(SenioresAtribuidos, QtdProjetos),% Lista com a alocação de Séniores aos Projetos (Recurso limitado).
         
+      
+        % Definição das variáveis de domninio do problema.        
+        domain(DatasInicio, 1, DataTermino),
+        domain(DatasFim, 1, DataTermino),
+        domain(DuracaoProjetos, 1, DataTermino),
         
-        
-        
-        
-        
-        length(LS,QtdEnc),
-        length(LD,QtdEnc),
-        length(LR,QtdEnc),
-        %LS = [1, 2, 3, 6, 7], % start times
-        domain(LS,1,10),
-        LD = [3, 9,10, 6, 2],% duration times
-        LR = [1, 2, 1, 1, 3],% resource used by tasks
-        % LE = [4,11,13,12, 9],
 
-        Limit in 1..8, % we can't use more than 8 resources at the
-                       % same time. In fact, we only use 7 in this
-                       % example.
+        % Restrições ao problema.
+        MaximoDeProjetos in 1..SenioresDisponiveis,
 
-        % setup a list of end times LE
-        ( foreach(S,LS),
-          foreach(D,LD),
-          foreach(K,LE) do
-              K #= S+D
+        
+        % Preparação das datas de fim dos projetos
+        ( foreach(DI, DatasInicio),
+          foreach(DP, DuracaoProjetos),
+          foreach(DF, DatasFim) do
+              DF #= DI + DP
         ),
+        
+        
+        % Atribuição de Séniores.
+        ( foreach(SA, SenioresAtribuidos) do
+                SA #= 1
+        ),        
 
-        % latest end time of all tasks, to minimize
-        maximum(End,LE), 
+        % Pretende-se terminar os projetos o mais cedo possível.
+        maximum(Final, DatasFim), 
 
         (
-            foreach(S, LS),
-            foreach(D, LD),
-            foreach(E, LE),
-            foreach(R, LR),
-            foreach(task(S,D,E,R,0),Tasks)
+            foreach(DI, DatasInicio),
+            foreach(DP, DuracaoProjetos),
+            foreach(DF, DatasFim),
+            foreach(SA, SenioresAtribuidos),
+            foreach(task(DI, DP, DF, SA,0), Projetos)
         do
             true
         ),
 
-        cumulative(Tasks, [limit(Limit)]),
+        cumulative(Projetos, [limit(MaximoDeProjetos)]),
 
-        append(LS,LE, Vars1),
-        append(Vars1,[Limit], Vars),
+        % Preparação do labeling.
+        append(DatasInicio, DatasFim, Vars1),
+        append(Vars1,[MaximoDeProjetos], Vars),
 
-        labeling([minimize(Limit)], Vars),
+        % Atribuição de valores concretos às variáveis (labeling).
+        labeling([minimize(Final)], Vars),
 
-        write('start   ':LS),nl,
-        write(duration:LD),nl,
-        write(resource:LR),nl,
-        write('end     ':LE),nl,
-        write('limit ':Limit),nl,
-        write(max_end_time:End),nl,
+        % Apresentação de resultados.
+        write('Datas de inicio dos projetos ': DatasInicio),nl,
+        write('Datas de finalização dos projetos ': DatasFim),nl,
+        write('Duração dos projetos': DuracaoProjetos),nl,
+        write('Utilização máximo de Séniores': SenioresAtribuidos),nl,
+        write('Máximo de projetos simultâneos ': MaximoDeProjetos),nl,
+        write('Último projeto termina em ': Final),nl,
         fd_statistics.
