1a162fc5088b6a57d5b92773de65cb2b9d3dfdff
Paulo <paulo@fariareis.com>
2012-12-06 03:00:01 +0000

    Ficheiro com o tratamento dos dados.

--- src/exemplo3.pl
+++ src/exemplo3.pl
@@ -1,15 +1,3 @@
-:-use_module(library(clpfd)).
-:-use_module(library(lists)).
-
-schedule(Ss, End) :-
-        length(Ss, 7),
-        Ds = [16, 6,13, 7, 5,18, 4],
-        Rs = [ 2, 9, 3, 7,10, 1,11],
-        domain(Ss, 1, 30),
-        domain([End], 1, 50),
-        after(Ss, Ds, End),
-        cumulative(Ss, Ds, Rs, 13),
-        labeling([minimize(End)], [End|Ss]).
-
-after([], [], _).
-after([S|Ss], [D|Ds], E) :- E #>= S+D, after(Ss, Ds, E).
+ifthen(A, B) :-
+        C = 4,
+        A =:= 1 -> B = C; B = 2.
