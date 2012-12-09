eef8ac8395dea9b106b52094635ba595ea96efff
Miguel Rossi Seabra <ciberwizz@ciberwizz.(none)>
2012-12-09 12:22:26 +0000

    alocate now works with globals

--- src/mimi.pl
+++ src/mimi.pl
@@ -30,57 +30,175 @@
 :- use_module(library(clpfd)).
 :- use_module(library(lists)).
 
-%ex aceitar_enc([[P1],[P2],[P3]], PROJ, VARS).
-% filtrar as encomendas para projectos
-aceitar_enc(Enc, Projectos, VARS) :-
-        % aceita apenas alguns projectos
-	(
-            foreach( A , Aceitacoes),
-            foreach( E , Enc),
-            foreach( P , Projectos)
-            do
-               A in 0..1,
-               append(E,A,P)
-        ),
-       
-        sum(Aceitacoes,#>= , 1),
 
-        labeling( [], Aceitacoes).
-      
+mimi :- 
+        % Variáveis globais para teste.
+
+        CustoAtrasoDiario is 1,          % Percentagem do total do projeto a pagar por cada dia de atraso (A).
+        Atraso_MaximoDias is 60,         % Maximo de dias de atraso por projeto (Adias).
+        Atraso_MaximoCusto is 50,        % Percentagem máxima do valor em falta do projeto que pode ser perdido por atrasos (Amax).      
+        
+        LimiteProjetoMedio is 50000,     % Projetos acima deste valor são médio (Nmed). O valor não está definido no texto, foi escolhido pelo grupo.
+        LimiteProjetoComplexo is 100000, % Projetos acima deste valor são sempre complexos (Ng).
+        Senior_ProjetoComplexo is 50,    % Percentagem de dedicação do tempo de Senior necessária para cada projeto complexo (Dc).
+        Senior_Minimo is 33,             % Percentagem de dedicação do tempo de Senior necessária para qualquer projeto (Dmin).
+        EntregaIntermedia_Min is 10,     % Percentagem minima do n.º de linhas do projeto para entrega intermédia (Imin).
+        EntregaIntermedia_Max is 90,     % Percentagem maxima do n.º de linhas do projeto para entrega intermédia (Imax). 
+        
+        ValorLinha_Complexo is 35,       % Preço das linhas de projetos complexos (PUc).
+        ValorLinha_Simples is 25,        % Preço das linhas de projetos simples (PUs).
+        
+        Seniores_Permanentes is 1,       % Número de Seniores iniciais (P).
+        CustoContrato is 500,            % Custo de novo contrato (C).
+        OrdenadoJunior is 1000,          % Remuneração mensal de junior (Jr).
+        OrdenadoSeniorPerm is 4000,      % Remunerção mensal de Senior permanente (Srp).
+        OrdenadoSeniorCont is 3000,      % Remunerção mensal de Senior contratado (Src).
+        Produtividade is 35,             % Linhas de codigo por dia por programador (N).
+        Apredizagem is 15,               % Periodo de produtividade nula após contrato.
+        
+        Capital_Inicial is 200000,        % Capital inicial (E),
+        DespesasMensais is 5000,         % Outras despesas mensais fixas.
+        Ferias is 11,                    % periodo de ferias em cada 6 meses
+
+
+
+        GLOBAIS = [
+             CustoAtrasoDiario,       %  1 
+             Atraso_MaximoDias,       %  2
+             Atraso_MaximoCusto,      %  3
+             LimiteProjetoMedio,      %  4
+             LimiteProjetoComplexo,   %  5
+             Senior_ProjetoComplexo,  %  6
+             Senior_Minimo,           %  7
+             EntregaIntermedia_Min,   %  8 
+             EntregaIntermedia_Max,   %  9
+             ValorLinha_Complexo,     % 10
+             ValorLinha_Simples,      % 11
+             Seniores_Permanentes,    % 12
+             CustoContrato,           % 13
+             OrdenadoJunior,          % 14
+             OrdenadoSeniorPerm,      % 15 
+             OrdenadoSeniorCont,      % 16
+             Produtividade,           % 17
+             Apredizagem,             % 18
+             Capital_Inicial,         % 19 
+             DespesasMensais,         % 20        
+             Ferias                   % 21
+        ],
+
+
+
+        % Lista de encomendas para testes.
+        Encomendas = [
+              [100000, 1, 80, 100], [150000, 0, 170, 200], [50000, 0, 145, 150], [3000, 1, 340, 360], [1000, 1, 280, 300], 
+              [4000, 1, 140, 200]/*, [1500, 1, 186, 212], [2900, 1, 135, 150], [5400, 1, 300, 360], [2100, 1, 290, 300],
+              [3000, 1, 230, 270], [2300, 0, 150, 200], [1300, 1, 135, 150], [2700, 1, 300, 360], [7200, 1, 220, 300],
+              [2500, 1, 320, 360], [1700, 1, 170, 200], [3800, 1, 135, 150], %[1600, 1, 330, 360], %[2900, 1, 270, 300],
+%              [1500, 0, 260, 280], [600, 1, 190, 200], [5300, 1, 135, 150], [3100, 1, 300, 360], [300, 1, 250, 300],
+%              [3500, 1, 230, 270], [4500, 1, 140, 200], [6300, 0, 135, 150], [4300, 1, 300, 360], [100, 1, 220, 300],
+%              [350, 1, 10, 30], [450, 1, 14, 20], [600, 1, 23, 30], [1400, 1, 20, 30], [1000, 1, 22, 30],
+%              [550, 1, 20, 25], [850, 1, 15, 18], [900, 1, 13, 22], [1800, 1, 10, 20], [2000, 1, 22, 30],
+              [950, 1, 40, 60], [1450, 1, 40, 60], [1600, 1, 23, 35], [1500, 1, 20, 40], [1000, 1, 22, 34],
+              [1550, 1, 45, 55], [1850, 1, 35, 47], [1900, 1, 43, 52], [2400, 1, 35, 53], [500, 1, 22, 34],              
+              [100, 1, 3, 7]
+  */            ],
+        
+        % Criação das listas com os vários parâmetros de trabalho.
+        length(Encomendas, QtdEncomendas),              % Verificar quantos projetos são para escalonar.
+%        length(DatasInicio, QtdEncomendas),            % Lista com as datas de inicio dos projetos.
+        length(DatasFinaisContratadas, QtdEncomendas),  % Lista com as datas de fim dos projetos.
+%        length(DuracaoProjetos, QtdEncomendas),        % Lista com a duração dos projetos.
+        length(SenioresAAtribuir, QtdEncomendas),       % Lista com a alocação de Séniores aos Projetos (Recurso limitado).
+        length(Complexidade, QtdEncomendas),            % Lista com a complexidade de acordo com a encomenda.
+        length(LinhasDeCodigo, QtdEncomendas),          % Linhas de código do projeto.
+        length(DatasMeioContratadas, QtdEncomendas),    % Lista com as datas intercalares dos projetos.
+        
+        
+        % Ler as encomendas.
+        (
+          foreach([LC, CP, DM, DF], Encomendas),
+          foreach(LC, LinhasDeCodigo),
+          foreach(CP1, Complexidade),
+          foreach(DM, DatasMeioContratadas),
+          foreach(DF, DatasFinaisContratadas),
+          foreach(SA, SenioresAAtribuir),
+          foreach([LC, SA, DM, DF], ProjetosAceites) do     % Criar a lista de tarefas.
+                (LC >= LimiteProjetoComplexo -> CP1 = 1; CP1 = CP),     % Garante que o requisito de projetos acima de certa dimensão é sempre complexo.
+                (LC >= LimiteProjetoComplexo -> SA = 100;       % Atribuir Seniores de acordo com complexidade e número de linhas de código. 
+%                 CP =:= 1 -> SA = Senior_ProjetoComplexo; SA = Senior_Minimo),
+                 CP =:= 1 -> SA = 100; SA = 100)
+        ),
+%        write('Seniores a atribuir a cada projeto': SenioresAAtribuir), nl,
+%        write('Rentabilidade Máxima das Encomendas': RentabilidadeMaximaEncomendas), nl,
+        
+
+        % Restrições a aplicar de acordo com os requisitos:
+
+        
+        % Seniores disponiveis.
+        Seniores #= Seniores_Permanentes * 100,
+        escalonar(1000, Seniores, ProjetosAceites, DatasFim, DatasInicio, DatasMeio, DuracaoProjetos),
+%        escalonar(1000, Seniores, [[350, 40, 10, 20], [450, 20, 14, 20], [600, 80, 23, 40], [1400, 100, 20, 30], [1000, 80, 122, 160]], DatasFim, DatasInicio, DatasMeio, DuracaoProjetos),        
+        
+%        write('Datas de entrega': DatasFim), nl,
+%        write('Datas de inicio': DatasInicio), nl,
+%        write('Datas intecalares': DatasMeio), nl,
+%        write('Duração dos projetos': DuracaoProjetos), nl,
+        
+        
+       
+
+
+
+
+%ENC = [[+Nlinhas,+NSeniors, +Data_It, +Data_F_proj, +Complexidade],..]
+
+        ENC = [[10000,50,0,12,1],[8000,50,0,12,1]],
+        Aceites = [1,1],
+
+        alocate(GLOBAIS,ENC,Aceites,Lucro,VARS).
+
+
+% consult('mimi.pl'). mimi.
 
 
 %hardcoded globals, just to test ideas
-%ENC = [[+Nlinhas,+NSeniors, +Data_It, +Data_F_proj, Aceite],..]
+%ENC = [[+Nlinhas,+NSeniors, +Data_It, +Data_F_proj, +Complexidade],..]
 
 % TODO allocate guys to projects
-   %TODO bug when there is less than one year of coding of a programmer
+   %TODO bug when there is less than one year of coding of a programmer, use domain?
 % TODO verificar atrasos
+% TODO Calcular a soma dos lucros
+% TODO alterar o preco das linhas de complexo  ou simples.
 
-% consult('mimi_testes.pl'). alocate( [[10000,1,0,12,1],[8000,1,0,12,1]], Lucro, VARS ).
-alocate( ENC, Lucro, VARS ) :-
-	E = 200*1000, % capital inicial	
-	C = 500, % custo de novo contracto
-	Srp = 4000, % ordenado senior
-	Jr = 1000, % ordenado Jr
-	A = 1,  % percentagem a decontar no lucro por dia de atraso
-	Amax = 20, % percentagem max de desconto
-	Adias = 21, % dias max de atraso
-        N =  35, % producao de linhas de codigo num dia por programador 
-	M = 1000, % despesas mensais
-	PUc = 25, % preco por linha de codigo num projecto complexo
-	Z = 15, % dias apos contratacao em que n produz
-        F = 11, % ferias por cada 6 meses
+% consult('mimi_testes.pl'). alocate( [[10000,100,0,12,1],[8000,1,0,12,1]],[1,1], Lucro, VARS ).
+alocate( Globals, ENC, Enc_aceites, Lucro, VARS ) :- write(Globals),nl,
+	nth1(19,Globals,E) ,    % capital inicial	
+	nth1(13,Globals,C),     % custo de novo contracto
+	nth1(15,Globals,Srp),   % ordenado senior permanente
+	nth1(14,Globals,Jr),    % ordenado Jr
+	nth1(1, Globals,A),     % percentagem a decontar no lucro por dia de atraso
+	nth1(3, Globals,Amax),  % percentagem max de desconto
+	nth1(2, Globals,Adias), % dias max de atraso
+        nth1(17,Globals,N),     % producao de linhas de codigo num dia por programador 
+	nth1(20,Globals,M),     % despesas mensais
+	nth1(10,Globals,PUc),   % preco por linha de codigo num projecto complexo
+        nth1(11,Globals,PUs),   % preco por linha de codigo simples
+	nth1(18,Globals,Z),     % dias apos contratacao em que n produz
+        nth1(21,Globals,F),     % ferias por cada 6 meses
+        nth1(12,Globals,NSr),   % numero de seniors existentes
 	
 
 
-        % despesas mensais durante um ano
-        Desp_Fixas_ano is 12*M, 
+        % despesas mensais durante um ano e ordenados dos Srs 
+        % inclui-se aqqui pos os Nseniors nas enc estao em percentagem
+        Desp_Fixas_ano is 12*(M + NSr*Srp), 
         Cap_projectos is E - Desp_Fixas_ano,
-
+write('Cap_projectos ':Cap_projectos),nl,
         % os orcamentos podem ir de 1e ate ao capital
         % mas a soma dos orcamentos dos projectos nao ultrapassa o capital inicial
         (
-            foreach([Nlinhas, Nseniors, Data_it, Data_F, Aceite], ENC),
+            foreach(Aceite, Enc_aceites),
             foreach( OT , OrcT),
             foreach( O , Orcamento)
             do
@@ -90,24 +208,30 @@ alocate( ENC, Lucro, VARS ) :-
         sum(Orcamento, #=< , Cap_projectos),
 
 
+
         (
             foreach( Orc, Orcamento),
-            foreach( [Nlinhas, Nseniors, Data_it, Data_F, Aceite], ENC),
-            foreach( [Contr,Nlp], Contractos),
+            foreach( [Nlinhas, Nseniors, Data_it, Data_F, Complex], ENC),
+            foreach( Aceite, Enc_aceites),
+            foreach( [Contr,Nlp,Nlps], Contractos),
             foreach( [Njuniors, Despesas,Receitas, Lucro_it, Lucro_F], Projectos)
             do
                 %quantidade de contratos por junior
                 Contr #= Aceite*(Data_F/6),
 
-                %numero de linhas que cada programador produz durante o projecto
+                %numero de linhas que cada programador Jr produz durante o projecto
                 Nlp #= Aceite*( (4*5*Data_F - Z - F*Contr)*N  ),
 
-                %numero de juniores e' o numero de programadores necessarios para 
-                % programar Nlinhas menos os senior que tb sao programadores 
-                Njuniors #= Aceite*(Nlinhas/Nlp),%-Nseniors),
+                %numero de linhas que cada programador sr produz durante o projecto
+                % nsenior e' a percentagem que o Sr esta no proj
+                Nlps #= Aceite*( (4*5*Data_F - F*Contr)*(Nseniors*N/100) ),
 
-                %despesas sao os ordenados + custos de contrato
-		Despesas #= Aceite*(C*Njuniors + Jr*Contr*6*Njuniors + Nseniors*Data_F*Srp),
+                %numero de juniores e' o numero de programadores necessarios para 
+                % programar as linhas q o  senior nao consegue produzir 
+                Njuniors #= Aceite*((Nlinhas-Nlps)/Nlp),
+
+               %despesas sao os ordenados + custos de contrato
+		Despesas #= Aceite*(C*Njuniors + Jr*Contr*6*Njuniors),
 
                 %as despesas nao podem ser mais q o orcamento
 		Despesas #=< Orc,
@@ -129,12 +253,119 @@ alocate( ENC, Lucro, VARS ) :-
       append( OrcT, Orcamento, VARS1),
       append(VARS1,VARS3,VARS),
 
+write(VARS),nl,
       labeling([], VARS),
 
       write('\n\n\nDADOS\n'),
+      write('Enc ':ENC),nl,
       write('Contractos ':Contractos),nl,
       write('Projectos ':Projectos),nl,
       write('Orcamento ':Orcamento),nl,
       write('OrcT ': OrcT), nl.
+
+
+
+
+
+% funcao com o objectivo de manipular os dados recebidos do escalonamento
+% de forma a que a funcao de alocamento consiga usa-los.
+
+date_to_duration(Proj, Data_I, Complexidade, ENC_aloc) :-      
+
+     (
+          foreach( [ NLinhas, NSr, Data_m, Data_f] , Proj),
+          foreach( DI, Data_I),
+          foreach( C , Complexidade),
+          foreach( [ NLinhas, NSr, Dur_m, Dur_f, C], ENC_aloc)
+          do
+               Dur_m #= Data_m - DI,
+               Dur_f #= Data_f - DI
+     ),
+
+     true. 
+
+
+
+
+
+% Escalonamento (scheduling) dos projetos.
+% DataTermino = Data limite para terminar todos os projetos.
+% SenioresDisponiveis = Quantidade de Séniores disponíveis a cada momento (se for utilizada percentagem, sendo que 1 Senior = 100, é possÃ­vel atribuir tempo parcial de Senior a projetos).
+% Projetos = Lista com os projetos aceites.
+% escalonar(1000, 4, [[350, 1, 10, 30], [450, 1, 14, 20], [600, 1, 23, 40], [1400, 1, 20, 30], [1000, 1, 22, 60]], DatasFim, DatasInicio, DatasMeio, DuracaoProjetos).
+% escalonar(1000, 400, [[350, 40, 10, 30], [450, 20, 14, 20], [600, 80, 23, 40], [1400, 100, 20, 30], [1000, 80, 22, 60]], DatasFim, DatasInicio, DatasMeio, DuracaoProjetos).
+escalonar(DataTermino, SenioresDisponiveis, Projetos, DatasFim, DatasInicio, DatasMeio, DuracaoProjetos) :-
+
+        % Criação das listas com os vários parâmetros.
+        length(Projetos, QtdProjetos),          % Verificar quantos projetos são para escalonar.
+        length(DatasInicio, QtdProjetos),       % Lista com as datas de inicio dos projetos.
+        length(DatasFim, QtdProjetos),          % Lista com as datas de fim dos projetos.
+        length(DuracaoProjetos, QtdProjetos),   % Lista com a duração dos projetos.
+        length(SenioresAtribuidos, QtdProjetos),% Lista com a alocação de Séniores aos Projetos (Recurso limitado).
+        length(LinhasDeCodigo, QtdProjetos),    % Linhas de código do projeto.
+        length(DatasMeio, QtdProjetos),         % Lista com as datas intercalares dos projetos.
+        length(Aceitacoes, QtdProjetos),
+        
+        % Definição das variáveis de dominio do problema.        
+        domain(DatasInicio, 1, DataTermino),
+        domain(DatasFim, 1, DataTermino),
+        domain(DuracaoProjetos, 1, DataTermino),
+        domain(Aceitacoes, 0, 1),
+        
+        
+        % leitura dos dados dos projetos.
+        (
+          foreach([LC, SA, DM, DF], Projetos),
+          foreach(LC, LinhasDeCodigo),
+          foreach(SA, SenioresAtribuidos),
+          foreach(DM, DatasMeio),
+          foreach(DF, DatasFim),
+          foreach(DP, DuracaoProjetos),
+          foreach(DI, DatasInicio),
+          foreach(AC, Aceitacoes),
+          foreach(SA, SenioresAtribuidos),
+          foreach(task(DI, DP, DF, Res, 0), ProjetosAEscalonar) do
+                DP #= DF - DI,
+                Res #= SA * AC
+        ),
+
+        
+        % Restrições ao problema.
+%        maximum(Final, DatasFim), 
+        
+        MaximoDeProjetos in 1..SenioresDisponiveis,
+        sum(Aceitacoes, #>=, 5),
+        count(1, Aceitacoes, #=, Aceites),
+        minimum(Inicio, DatasInicio),
+        
+        % Preparação do labeling.
+        append(DatasInicio, DatasFim, Vars1),
+        append(Vars1, [MaximoDeProjetos], Vars),
+        
+                
+        % obtenção de soluções
+        cumulative(ProjetosAEscalonar, [limit(MaximoDeProjetos)]), % Processamento para obtenção do escalonamento de tarefas.
+        
+        labeling([down], Aceitacoes),
+        labeling([up], DuracaoProjetos),
+        labeling([], Vars),      % Atribuição de valores concretos Ã s variáveis (labeling).    
+        
+        
+        % Apresentação de resultados.
+        write('Datas de inicio dos projetos ': DatasInicio),nl,
+        write('Datas de entrega intercalar dos projetos ': DatasMeio),nl,
+        write('Datas de finalização dos projetos ': DatasFim),nl,
+        write('Duração dos projetos': DuracaoProjetos),nl,
+        write('Aceitações': Aceitacoes), nl,
+        write('Linhas de código dos projetos': LinhasDeCodigo), nl,
+        write('Utilização máximo de Séniores': SenioresAtribuidos),nl,
+        write('Máximo de projetos simultâneos ': MaximoDeProjetos),nl,
+        write('Recursos utilizados': SenioresAtribuidos), nl,
+        write('Projetos escalonados': ProjetosAEscalonar), nl,
+        write('Projetos aceites': Aceites), nl,
+        write('Inicio': Inicio), nl,
+        fd_statistics.
+
+
 
 
